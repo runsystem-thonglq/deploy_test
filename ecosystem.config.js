69ef959dc47920ec9ff86ae87642ac757188b7c6
thonglq <thonglq@runsystem.net>
2024-09-06 11:06:18 +0700

    chore: init

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      script: "npm start",
+    },
+  ],
+
+  deploy: {
+    production: {
+      key: "key.pem",
+      user: "ubuntu",
+      host: "SSH_HOSTMACHINE",
+      ref: "origin/main",
+      repo: "GIT_REPOSITORY",
+      path: "/home/ubuntu",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+      ssh_options: "ForwardAgent=yes",
+    },
+  },
+};
